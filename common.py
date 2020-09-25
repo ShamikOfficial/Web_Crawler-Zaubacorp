6040af259a08edf0052b28501eb9d29aa7ac1c23
Shamik <shamik1900@gmail.com>
2020-09-25 22:24:39 +0530

    updated engine.py
    Major changes by adding best search function and comman line arguments for user input with relevent comments.

--- common.py
+++ common.py
@@ -2,17 +2,21 @@ import json
 import pandas as pd
 import os
 
-#Check and make output directory 
-def makedirs():   
+#Check and make output directory
+def makedirs():
     if not os.path.exists('output'):
         os.mkdir('output')
 
 #function to store individual company data in xlsx format
 def store_indi(outf,groupedlist1):
     makedirs()
-    writer = pd.ExcelWriter('output'+os.path.sep+outf, engine='xlsxwriter')
-    for sheet0,list1 in groupedlist1:
-        df1 = pd.DataFrame(data = list1,columns=None)
-        df1.to_excel(writer, sheet_name=sheet0,index=False,header=False)
+    try:
+        writer = pd.ExcelWriter('output'+os.path.sep+outf, engine='xlsxwriter')
+        for sheet0,list1 in groupedlist1:
+            df1 = pd.DataFrame(data = list1,columns=None)
+            df1.to_excel(writer, sheet_name=sheet0,index=False,header=False)
+        print("Succesfully saved data in Output Folder")
+    except:
+        print("Some error occured in Saving File")
 
     writer.save()
--- engine.py
+++ engine.py
@@ -12,53 +12,85 @@ def parser(url):
 
 #Initialize scrapping by getting required infi from config json file to extract data
 def crawler():
-    with open("config.json") as jdata:            #reading json file
-        data = json.load(jdata)
+    with open("config.json") as jdata:          #reading json file
+        data = json.load(jdata)                 #calling json objects
+        search_pg=data[0]["html"]               #Search link retrieved from config.json
+        #outf=data[0]["Outfile_name"]           #Sample data for dev
+        url=user_choice(search_pg)              #get user's required link to scrap
+        groupedlist1=Extract_Zauba_indi(url)                     #final individual company data packed into list of lists
+        store_indi(str(url.split("/")[-2]+".xlsx"),groupedlist1)       #storing individual company data in excel
+        print("Scrapping completed Succesfully!!!")
 
-        #calling json objects
-        html=data[0]["html"]
-        company_name="BYJU-S-K3-EDUCATION-PRIVATE-LIMITED"
-        company_cin="U80900KA2018PTC115288"
-        start_pg=data[0]["start_page"]
-        stop_pg=data[0]["stop_page"]
-        outf=data[0]["Outfile_name"]
-        url=str(html+company_name+"/"+company_cin)
-        groupedlist1=Extract_Zauba_indi(url)          #final individual company data packed into list of lists
-        store_indi(str(company_name+".xlsx"),groupedlist1)       #storing individual company data in excel
-        #print(all_list)
+#Command Line Choices Display
+def user_choice(search_pg):
+    print("Welcome to Speed Crawler")
+    mode=0
+    url=""
+    mode=int(input("Please enter required mode number from options below:\n1 - Download Single Data by CIN number or Top Search\n2 - Download Single Data by Exact Zaubicorp Link\n\nChoice: "))
+    if (mode==1):
+        search_key=str(input("Enter Search Keyword or CIN number: "))
+        url=zauba_top_search(search_pg,search_key)
+        return url
+    if (mode==2):
+        url=str(input("Enter exact Zaubacorp link: "))
+        return url
+    else:
+        print("Exiting program as no suitable response recieved")
+        exit()
+
+#Returns the Best Search Link
+def zauba_top_search(search_pg,search_key):
+    #search_key="sun" #sample search for devs
+    top_link=""
+    soup2=""
+    company_link=search_pg+search_key
+
+    try:
+        soup2=parser(company_link)
+        search_data=(soup2.find("div",{"class":"col-xs-12"}))
+        top_link =search_data.find_all("table")[0].find("a").get("href")    #GET FIRST SEARCH LINK
+        print("\nScrapping initiated . . .")
+        return str(top_link)
+    except:
+        print("No search result found for "+search_key)
+        exit()
 
 #Pack the details of a specific company in a list.
 def Extract_Zauba_indi(url):
-    soup=parser(url)    #calling nested function
-    #details=(soup.find_all("div",{"class":"container information"}))
-    details=(soup.find_all("div",{"class":"col-lg-12 col-md-12 col-sm-12 col-xs-12"}))
-    #print(details)
-    i=0
-    #Scraping all tabulated Company Details from Zauba Corp
-    groupedlist1=[]
-    for i in range(20):
-        list1=[]
-        list2=[]
-        field1=[]
-        field2=[]
-        try:
-            field1 = details[i].find_all("table")[0].find_all("tr")
-            field2 = details[i].find_all("h4")[0].text.strip()  #Name of dataframe
-        except:
-            continue
-        for element in field1:
-            sub_data = []
-            for sub_element in element:
-                try:
-                    sub_data.append(sub_element.text.strip())
-                except:
-                    continue
-            list1.append(sub_data)
-        list2.append(field2[:29])
-        list2.append(list1)
-        groupedlist1.append(list2)
-        print(field2,"\n",list1)
-    return groupedlist1
+    try:
+        soup=parser(url)    #calling nested function
+        details=(soup.find_all("div",{"class":"col-lg-12 col-md-12 col-sm-12 col-xs-12"}))
+        #print(details)
+        i=0
+        #Scraping all tabulated Company Details from Zauba Corp
+        groupedlist1=[]
+        for i in range(20):
+            list1=[]
+            list2=[]
+            field1=[]
+            field2=[]
+            try:
+                field1 = details[i].find_all("table")[0].find_all("tr")     #Table Contents
+                field2 = details[i].find_all("h4")[0].text.strip()          #Name of dataframe
+            except:
+                continue
+            for element in field1:
+                sub_data = []
+                for sub_element in element:
+                    try:
+                        sub_data.append(sub_element.text.strip())
+                    except:
+                        continue
+                list1.append(sub_data)
+            list2.append(field2[:29])   #name of table scrapped to save as sheet name - less than 32 characters
+            list2.append(list1)         #fial data without table name
+            groupedlist1.append(list2)  #final list of data scraped with table name
+            #print(field2,"\n",list1)
+        print("Succesfully Extracted Data")
+        return groupedlist1
+    except:
+        print("Oops, No matching Results, exiting Scrapper")
+        exit()
 
 
 
